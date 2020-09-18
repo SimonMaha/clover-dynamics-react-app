bca401156d55673bdf664f77024ab199040a56a9
Sviatoslav Maha <svatoslavmaga@gmail.com>
2020-09-18 07:12:16 +0300

    Updates

---
+++ precache-manifest.2a50f9a20f58ed65644b3abe1d0b9ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e922731ff36384ad243b8acc8d14d7",
+    "url": "/clover-dynamics-react-app/index.html"
+  },
+  {
+    "revision": "e9fbd55f87ce7cac753e",
+    "url": "/clover-dynamics-react-app/static/css/main.c0e9805a.chunk.css"
+  },
+  {
+    "revision": "4f75b3aa737cd090b1fc",
+    "url": "/clover-dynamics-react-app/static/js/2.386ff5a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clover-dynamics-react-app/static/js/2.386ff5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fbd55f87ce7cac753e",
+    "url": "/clover-dynamics-react-app/static/js/main.9e561e54.chunk.js"
+  },
+  {
+    "revision": "4bfffdf7966f098977a2",
+    "url": "/clover-dynamics-react-app/static/js/runtime-main.7c0e7137.js"
+  }
+]);
